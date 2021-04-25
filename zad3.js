b467d01c6b68499374d5501167a17aa5df2395ab
Maria Malasiewicz <malasie@users.noreply.github.com>
2021-04-25 16:41:48 +0200

    wkońcu coś wychodzi

--- zad3.js
+++ zad3.js
@@ -1,9 +1,11 @@
-const imie = "Maria";
-const nazwisko = 'Małasiewicz';
-const email;
-
-if (imie.charAt(0)==imie.charAt(0).toUpperCase && imie.substr(1)==imie.substr(1).toLowerCase);
-if (nazwisko.charAt(0)==nazwisko.charAt(0).toUpperCase && nazwisko.substr(1)==nazwisko.substr(1).toLowerCase);
-
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+function f(){
+    var imie = input1.value
+    var nazwisko=input2.value
+    const litery= /^[a-zA-Z ]{3,}$/g;
+    if (imie[0]==imie[0].toUpperCase() && imie.slice(1)==imie.slice(1).toLowerCase() && litery.test(imie)) {
+        alert('ALL GOOD!')
+    }
+    else {alert('Nieprawidlowe imie ')}
+    if (nazwisko.charAt(0)==nazwisko.charAt(0).toUpperCase() && nazwisko.substr(1)==nazwisko.substr(1).toLowerCase(), litery.test(nazwisko));
+    else {alert('Nieprawidłowo wprowadzone nazwisko')}
+}
