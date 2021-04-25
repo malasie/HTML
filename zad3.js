bae97a93916607311460f1918225e8246414572e
Maria Malasiewicz <malasie@users.noreply.github.com>
2021-04-25 14:57:29 +0200

    1

---
+++ zad3.js
@@ -0,0 +1,9 @@
+const imie = "Maria";
+const nazwisko = 'Małasiewicz';
+const email;
+
+if (imie.charAt(0)==imie.charAt(0).toUpperCase && imie.substr(1)==imie.substr(1).toLowerCase);
+if (nazwisko.charAt(0)==nazwisko.charAt(0).toUpperCase && nazwisko.substr(1)==nazwisko.substr(1).toLowerCase);
+
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
