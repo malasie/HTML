c818d284a48c20890e9b130e4fac26fcb8a33e43
Maria Malasiewicz <malasie@users.noreply.github.com>
2021-05-09 13:51:36 +0200

    lets go

--- zad3/skr2.js
+++ zad3/skr2.js
@@ -1,11 +1,16 @@
 function sprawdz(){
     var liczby = input1.value
-    var a=0; 
+    var max=0; 
+    var a=0;
+
     for( i=0; i<liczby.length; i++){
         if(isNaN(liczby[i])!=true){
-            if(a<liczby[i]){a=liczby[i]}
+            a++;
+            if(max<liczby[i]){max=liczby[i]}
         }
     }
-    odpowiedz.setT
-    alert('Najwieksza cyfra: '+ a)
+    if(a==7){
+        odpowiedz.value=max
+    }
+    else {alert("wprowadzono "+a+" liczb. Wprowadz 7 liczb.")}
 }
